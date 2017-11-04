f15ceade3a556ba5255e53bfba05c90cb4ea2821
Hejle <johej15@student.sdu.dk>
2017-11-04 19:11:37 +0100

    Removed Holdgæft

--- Pieces.js
+++ Pieces.js
@@ -3,14 +3,24 @@ class Position {
         this.xpos = xpos;
         this.ypos = ypos;
     }
-    get getPosition() {
-        return this.xpos+this.ypos;
+    get pos() {
+        return (this.xpos+this.ypos).toString();
+    }
+    get yGet() {
+        return this.xpos;
+    }
+    get xGet() {
+        return this.xpos;
     }
 }
 
 class Team {
     constructor(team) {
         this.team = team;
+    }
+
+    get getTeam() {
+        return this.team;
     }
 }
 
@@ -20,6 +30,54 @@ class Pawn {
         this.position = Position;
     }
 
+        /*
+        checkUpgrade() {
+            if (this.team === "White" && this.position.getYPosition == 8) {
+            //    upgrade();
+            } else (this.team === "Black" && this.position.getYPosition == 1) {
+          //      upgrade();
+            }
+        }
+
+        upgrade() {
+            new Queen(this.position.getPosition, this.team.getTeam);
+        } */
 }
 
-var a = new Pawn(new Position("a","2"), new Team("White"));
+var a = new Pawn(new Position("A", 2), new Team("White"));
+
+
+class Tower {
+    onstructor(Position, Team){
+        this.team = Team;
+        this.position = Position;
+    }
+}
+
+class Knight {
+    onstructor(Position, Team){
+        this.team = Team;
+        this.position = Position;
+    }
+}
+
+class Bishop {
+    onstructor(Position, Team){
+        this.team = Team;
+        this.position = Position;
+    }
+}
+
+class Queen {
+    onstructor(Position, Team){
+        this.team = Team;
+        this.position = Position;
+    }
+}
+
+class King {
+    onstructor(Position, Team){
+        this.team = Team;
+        this.position = Position;
+    }
+}
