96124f570505b3ab5622f307fc5f14f1cd6042a4
Hejle <johej15@student.sdu.dk>
2017-11-04 20:18:41 +0100

    Removed Holdgæft

--- Pieces.js
+++ Pieces.js
@@ -15,14 +15,18 @@ class Position {
         return this.xpos;
     }
 
-    setY(yPos) {
-        this.ypos = yPos;
+    setY(ypos) {
+        this.ypos = ypos;
     }
 
-    set geru(pitjh) {
-        this.ypos = pitjh;
+    setX(xpos) {
+        this.xpos = xpos;
     }
 
+    setPos(Position) {
+        this.xpos = Position.getX();
+        this.ypos = Position.getY();
+    }
 }
 
 class Team {
@@ -42,14 +46,14 @@ class Pawn {
     }
 
     upgrade() {
-        return new Queen(this.position, new Team(this.team.teamGet));
+        return new Queen(new Position(this.position.getX(), this.position.getY()), new Team(this.team.getTeam()));
     }
 
     checkUpgrade() {
         if (this.team === "White" && this.position.getY == 8) {
-            upgrade();
+            this.upgrade();
         } else if (this.team === "Black" && this.position.getY == 1) {
-            upgrade();
+            this.upgrade();
         }
     }
 
@@ -90,14 +94,3 @@ class King {
         this.position = Position;
     }
 }
-var a = new Pawn(new Position("A", 2), new Team("White"));
-var b = a.upgrade();
-console.log(a.position.getPos() + a.team.getTeam());
-a.position.setY(3);
-console.log(a.position.getPos() + a.team.getTeam());
-
-/*
-console.log(b.position.pos + b.team.teamGet);
-delete a;
-console.log(b.position.pos + b.team.teamGet);
-*/
