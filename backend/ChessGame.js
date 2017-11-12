ce027487af2be8d8b83c301d7bd1b0d9c238cd4b
Hejle <johej15@student.sdu.dk>
2017-11-12 10:11:58 +0100

    resetBoard and isGameOver

--- backend/ChessGame.js
+++
@@ -1,9 +0,0 @@
-
-var x = document.createElement('script');
-x.src = "Pieces.js";
-document.getElementsByTagName("head")[0].appendChild(x);
-
-var pawn1 = new Pawn(new Position(B,2),"White");
-
-console.log(pawn1);
-
